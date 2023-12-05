1c90df7e71ffafe1e057fce1ab951d3dc299c943
Filipp Kosenko <filipp.kosenko@idapgroup.com>
2023-12-05 16:52:24 +0200

    Added fonts, images. Created dashboard screen logic and some layout. Added navigation.

--- lib/ui/dashboard_screen/dashboard_screen.dart
+++ lib/ui/dashboard_screen/dashboard_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -8,41 +10,176 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  
+  final _pageController = PageController(
+    initialPage: 0,
+    viewportFraction: 1.1
+  );
+  final _pagesCount = 3;
+  var _currentPage = 0;
+  final bgImage = Image(image: AssetImage('assets/images/dashboard/dashboard_bg.png'));
+  final List<Widget> _images = [
+    const Image(image: AssetImage('assets/images/dashboard/dashboard_shadow.png')),
+    const Image(image: AssetImage('assets/images/dashboard/dashboard_purple.png')),
+    const Image(image: AssetImage('assets/images/dashboard/dashboard_all.png'))
+  ]; 
+  final Color gold = Color(0xFFFFD606);
+  final Color heather = Color(0xFFBAC4CE);
+  final Color abbey = Color(0xFF494F54);
+  final Color wildSand = Color(0xFFF5F5F5);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: wildSand,
       body: Container(
-        child: ListView.separated(
-        itemBuilder: (context, index) {
-          return dashboardPage(index);
-        },
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        padding: EdgeInsets.all(16),
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(width: 12);
-        },
-      ),
+        child: PageView.builder(
+          controller: _pageController,
+          scrollDirection: Axis.horizontal,
+          itemCount: _pagesCount,
+          onPageChanged: (index) { 
+            setState(() {
+              _currentPage = index;
+            }); 
+          },
+          itemBuilder: (context, index) {
+            return dashboardPage(index);
+          }
+        )
       ),
       bottomNavigationBar: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BackButton(),
-          BackButton()
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/list');
+            },
+            child: Text(
+              'Skip',
+              style: TextStyle(
+                color: gold,
+                fontFamily: 'Plus Jakarta Sans',
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ),
+          Spacer(),
+          pagerWidget(),
+          Spacer(),
+          _currentPage == 2 ? emptySpace() : nextButton()
         ],
       )
     );
   }
 
-  Widget dashboardPage(int index) => Container(
-    width: MediaQuery.of(context).size.width - 32,
-    height: MediaQuery.of(context).size.height - 150,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(16),
-      color: Colors.grey,
+  Widget nextButton() => TextButton(
+    onPressed: () {
+      _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.linear);
+    }, 
+    child: Text(
+      'Next',
+      style: TextStyle(
+        color: gold,
+        fontFamily: 'Plus Jakarta Sans',
+        fontWeight: FontWeight.bold
+      ),
     ),
   );
 
+  Widget emptySpace() => TextButton(
+    onPressed: null, 
+    child: Text(' ')
+  );
+
+  Widget dashboardPage(int index) => Container(
+    alignment: Alignment.bottomCenter,
+    padding: const EdgeInsets.only(left: 16,right: 16, top: 150, bottom: 10),
+    child: FractionallySizedBox(
+      widthFactor: 1 / _pageController.viewportFraction,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: wildSand
+        ),
+        child: Column(
+          children: [
+          Stack(
+            children: [
+              bgImage,
+              _images[index]
+            ],
+          ),
+          Spacer(),
+          Container(
+            child: SizedBox(
+              height: 65,
+              width: 200,
+              child: Visibility(
+                visible: index == 2 ? true : false,
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0)
+                      )
+                    ),
+                    backgroundColor: MaterialStateProperty.all(gold)
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/list');
+                  },
+                  child: Text(
+                    'GO!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                      backgroundColor: gold
+                    ),
+                  ),
+                ),
+              )
+            )
+          )
+          ]
+        )
+      )
+    )
+  );
+
+  Widget pagerWidget() => Container(
+    height: 100,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:
+        List<Widget>.generate(
+          _pagesCount, (index) => Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: InkWell(
+              onTap: () {
+                _pageController.animateToPage(index, duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: _currentPage == index
+                            ? abbey
+                            : wildSand,
+                  border: Border.all(
+                    color: _currentPage == index
+                            ? abbey
+                            : heather,
+                    width: 2
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                height: 16.0,
+                width: 16.0,
+                child: Center(
+                    // Your Widget
+                ),
+              ),
+            ),
+          ),
+        ),
+    ),
+  );
 }
