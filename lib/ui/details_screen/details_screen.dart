cc23b4cbefbdc7ddee16774cbd23ca09312504a3
Filipp Kosenko <filipp.kosenko@idapgroup.com>
2024-01-14 19:43:34 +0200

    Details screen app bar layout updated.

--- lib/ui/details_screen/details_screen.dart
+++ lib/ui/details_screen/details_screen.dart
@@ -16,7 +16,6 @@ class DetailsScreen extends StatefulWidget {
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
-  
   @override
   void initState() {
     // TODO: implement initState
@@ -30,27 +29,48 @@ class _DetailsScreenState extends State<DetailsScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        actions: const <Widget>[],
-        surfaceTintColor: ColorConstants.wildSand,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text(
+              '#' + pokemon.id.toString().padLeft(3, '0'),
+              style: TextStyle(
+                fontFamily: 'Plus Jakarta Sans',
+                fontWeight: FontWeight.w800,
+                fontSize: 24,
+                color: Colors.white
+              ),
+            ),
+          )
+        ],
+        backgroundColor: _typeModelList(pokemon.types).first.textColor,
         title: const Text(
           'Base experience',
           style: TextStyle(
               fontFamily: 'Plus Jakarta Sans',
-              fontWeight: FontWeight.bold,
-              fontSize: 15),
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+              color: Colors.white),
         ),
-        backgroundColor: ColorConstants.wildSand,
+        leading: IconButton(
+            padding: EdgeInsets.all(15),
+            onPressed: () => Navigator.pop(context),
+            icon: Image.asset('assets/images/icon_buttons/back_arrow.png')),
+        scrolledUnderElevation: 0,
       ),
       body: Container(
-          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: _typeModelList(pokemon.types).first.textColor,
+              gradient: LinearGradient(colors: [
+                _typeModelList(pokemon.types).first.textColor ?? Colors.black,
+                Colors.white
+              ], begin: Alignment.topCenter, end: Alignment.center)),
           alignment: Alignment.center,
           child: Column(
             children: [
               DecoratedBox(
                 decoration: BoxDecoration(
-                    color: _typeModelList(pokemon.types)
-                          .first
-                          .backgroundColor,
+                    color: _typeModelList(pokemon.types).first.backgroundColor,
                     borderRadius: BorderRadius.circular(100)),
                 child: Image(
                   fit: BoxFit.fill,
@@ -59,63 +79,78 @@ class _DetailsScreenState extends State<DetailsScreen> {
                   image: NetworkImage(pokemon.sprites.frontDefault),
                 ),
               ),
+              Container(
+                height: 16,
+              ),
               Expanded(
-                  child: Column(
-                children: [
-                  Text(
-                    pokemon.name,
-                    style: const TextStyle(
-                        fontFamily: 'Paytone One',
-                        fontWeight: FontWeight.w800,
-                        fontSize: 40),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    color: Colors.white,
                   ),
-                  ChipView(
-                    format: ChipViewFormat.imageAndText, 
-                    items: _typeModelList(pokemon.types),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Spacer(),
-                      valueColumn('Height', pokemon.height.toString()),
-                      const Spacer(),
-                      valueColumn('Weight', pokemon.weight.toString()),
-                      const Spacer(),
-                      valueColumn('Order', pokemon.order.toString()),
-                      const Spacer()
-                    ],
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      padding: const EdgeInsets.only(top: 16, bottom: 16),
-                      itemCount: _modelMap(pokemon).length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                  padding:
+                      EdgeInsets.only(top: 8, bottom: 16, left: 16, right: 16),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Expanded(
+                        child: Column(
+                      children: [
+                        Text(
+                          pokemon.name.capitalizeFirst(),
+                          style: const TextStyle(
+                              fontFamily: 'Paytone One',
+                              fontWeight: FontWeight.w800,
+                              fontSize: 40,
+                              color: ColorConstants.abbey),
+                        ),
+                        Container(height: 6),
+                        ChipView(
+                          format: ChipViewFormat.imageAndText,
+                          items: _typeModelList(pokemon.types),
+                        ),
+                        Container(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(
-                              _modelMap(pokemon)[index]!.$1,
-                              style: const TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                fontWeight: FontWeight.w500,
-                                fontSize: 15,
-                                color: ColorConstants.heather
-                              ),
-                            ),
-                            ChipView(
-                              format: ChipViewFormat.textOnly, 
-                              items: _modelMap(pokemon)[index]?.$2 ?? []
-                            ),
-                            Container(
-                              height: 15
-                            )
+                            const Spacer(),
+                            valueColumn('Height', pokemon.height.toString()),
+                            const Spacer(),
+                            valueColumn('Weight', pokemon.weight.toString()),
+                            const Spacer(),
+                            valueColumn('Order', pokemon.order.toString()),
+                            const Spacer()
                           ],
-                        );
-                      },
-                    ),
-                  )
-                ],
-              )),
+                        ),
+                        ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.only(top: 16, bottom: 16),
+                          itemCount: _modelMap(pokemon).length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  _modelMap(pokemon)[index]!.$1,
+                                  style: const TextStyle(
+                                      fontFamily: 'Plus Jakarta Sans',
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 15,
+                                      color: ColorConstants.heather),
+                                ),
+                                ChipView(
+                                    format: ChipViewFormat.textOnly,
+                                    items: _modelMap(pokemon)[index]?.$2 ?? []),
+                                Container(height: 15)
+                              ],
+                            );
+                          },
+                        ),
+                      ],
+                    )),
+                  ),
+                ),
+              ),
             ],
           )),
     );
@@ -153,15 +188,28 @@ class _DetailsScreenState extends State<DetailsScreen> {
     return modelList;
   }
 
-  Map<int,(String, List<ChipModel>)> _modelMap(FullPokemon pokemon) {
+  Map<int, (String, List<ChipModel>)> _modelMap(FullPokemon pokemon) {
     var map = {
-      0:('Abilities', pokemon.abilities.map((e) => ChipModel(title: e.ability.name.capitalizeFirst())).toList()),
-      1:('Forms', pokemon.forms.map((e) => ChipModel(title: e.name.capitalizeFirst())).toList()),
-      2:('Moves', pokemon.moves.map((e) => ChipModel(title: e.move.name.capitalizeFirst())).toList())
+      0: (
+        'Abilities',
+        pokemon.abilities
+            .map((e) => ChipModel(title: e.ability.name.capitalizeFirst()))
+            .toList()
+      ),
+      1: (
+        'Forms',
+        pokemon.forms
+            .map((e) => ChipModel(title: e.name.capitalizeFirst()))
+            .toList()
+      ),
+      2: (
+        'Moves',
+        pokemon.moves
+            .map((e) => ChipModel(title: e.move.name.capitalizeFirst()))
+            .toList()
+      )
     };
-    
+
     return map;
   }
 }
-
-
