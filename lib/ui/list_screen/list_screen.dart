68180b5fe84dff75760d7a483fd169373d4350b7
Filipp Kosenko <filippkosgmc@gmail.com>
2024-02-01 16:56:02 +0200

    Refactoring

--- lib/ui/list_screen/list_screen.dart
+++ lib/ui/list_screen/list_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pokemon_app/const/color_constants.dart';
 import 'package:flutter_pokemon_app/models/full_pokemon_model.dart';
 import 'package:flutter_pokemon_app/services/debouncer.dart';
-import 'package:flutter_pokemon_app/services/network_service.dart';
 import 'package:flutter_pokemon_app/services/pagination.dart';
 import 'package:flutter_pokemon_app/ui/list_screen/single_column_cell.dart';
 import 'package:flutter_pokemon_app/ui/list_screen/twin_column_cell.dart';
@@ -19,20 +18,19 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final NetworkService networkService = NetworkService();
+
   final ScrollController _scrollController = ScrollController();
-  final TextEditingController _textEditingController = TextEditingController();
-  final Pagination paginationService = Pagination();
-  StreamController<bool> _searchBarController = StreamController<bool>();
+  final Pagination _paginationService = Pagination();
+  final _updateDataStreamController = StreamController<List<FullPokemon>>();
   final _searchDebouncer = Debouncer(milliseconds: 1500);
   SearchField? _searchField;
 
   List<FullPokemon> _commonFullPokemonList = [];
-  var _streamController = new StreamController<List<FullPokemon>>();
 
+  var _appBarRightIcon = Icons.grid_view;
   var _isGridEnabled = false;
   var _isLoading = false;
-  dynamic appBarRightIcon = Icons.grid_view;
+
   String? filter;
   int page = 10;
   int limit = 10;
@@ -40,9 +38,8 @@ class _ListScreenState extends State<ListScreen> {
 
   @override
   void dispose() {
-    _streamController.close();
+    _updateDataStreamController.close();
     _scrollController.dispose();
-    _textEditingController.dispose();
 
     super.dispose();
   }
@@ -51,7 +48,8 @@ class _ListScreenState extends State<ListScreen> {
   void initState() {
     super.initState();
 
-    _searchField = SearchField(pullToRefreshCallback: pullToRefresh, textEditingController: _textEditingController, stream: _searchBarController.stream);
+    _searchField = SearchField(pullToRefreshCallback: pullToRefresh);
+
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
               _scrollController.position.maxScrollExtent &&
@@ -60,14 +58,15 @@ class _ListScreenState extends State<ListScreen> {
       }
     });
 
-    _textEditingController.addListener(() {
+    _searchField?.textEditingController.addListener(() {
       _searchDebouncer.run(() {
-        String? text = _textEditingController.text;
-        text = text.isEmpty ? null : text;
-        text != null ? _searchBarController.add(true) : _searchBarController.add(false);
-        if (text != filter) {
-          resetData(text);
-          loadPokemons(filter);
+        String? text = _searchField?.textEditingController.text;
+        if (text != null) {
+          text = text.isEmpty ? null : text;
+          if (text != filter) {
+            resetData(text);
+            loadPokemons(filter);
+          }
         }
       });
     });
@@ -80,22 +79,22 @@ class _ListScreenState extends State<ListScreen> {
     limit = page;
     offset = 0;
     _commonFullPokemonList.clear();
-    _streamController.add(_commonFullPokemonList);
+    _updateDataStreamController.add(_commonFullPokemonList);
   }
 
   loadingInit() async {
-    await paginationService.prepareAllShortList();
+    await _paginationService.prepareAllShortList();
     loadPokemons(filter);
   }
 
   loadPokemons(String? filter) async {
     _isLoading = true;
-    _commonFullPokemonList = await paginationService.getPage(
+    _commonFullPokemonList = await _paginationService.getPage(
       filter, 
       limit, 
       _commonFullPokemonList.length
     );
-    _streamController.add(_commonFullPokemonList);
+    _updateDataStreamController.add(_commonFullPokemonList);
     limit += page;
     _isLoading = false;
   }
@@ -103,147 +102,151 @@ class _ListScreenState extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ColorConstants.wildSand,
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                appBarRightIcon,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                setState(() {
-                  _isGridEnabled = !_isGridEnabled;
-                  if (_isGridEnabled == true) {
-                    appBarRightIcon = Icons.view_agenda_outlined;
-                  } else {
-                    appBarRightIcon = Icons.grid_view;
-                  }
-                });
-              },
+      backgroundColor: ColorConstants.wildSand,
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              _appBarRightIcon,
+              color: Colors.black,
             ),
-          ],
-          surfaceTintColor: ColorConstants.wildSand,
-          title: const Text(
-            'All pokemon',
-            style: TextStyle(
-                fontFamily: 'Plus Jakarta Sans', fontWeight: FontWeight.bold),
+            onPressed: () {
+              setState(() {
+                _isGridEnabled = !_isGridEnabled;
+                if (_isGridEnabled == true) {
+                  _appBarRightIcon = Icons.view_agenda_outlined;
+                } else {
+                  _appBarRightIcon = Icons.grid_view;
+                }
+              });
+            },
           ),
-          backgroundColor: ColorConstants.wildSand,
-          leading: IconButton(
-              padding: EdgeInsets.all(15),
-              onPressed: () => Navigator.pop(context),
-              icon: Image.asset('assets/images/app_icon/android.png')),
-          scrolledUnderElevation: 0,
+        ],
+        surfaceTintColor: ColorConstants.wildSand,
+        title: const Text(
+          'All pokemon',
+          style: TextStyle(
+              fontFamily: 'Plus Jakarta Sans', fontWeight: FontWeight.bold),
         ),
-        body: Column(
-          children: [
-            _searchField!,
-            Expanded(child: bodyView())
-          ],
-        ));
+        backgroundColor: ColorConstants.wildSand,
+        leading: IconButton(
+            padding: EdgeInsets.all(15),
+            onPressed: () => Navigator.pop(context),
+            icon: Image.asset('assets/images/app_icon/android.png')),
+        scrolledUnderElevation: 0,
+      ),
+      body: Column(
+        children: [
+          _searchField!,
+          Expanded(child: bodyView())
+        ],
+      )
+    );
   }
 
   Widget bodyView() => StreamBuilder<List<FullPokemon>>(
-      stream: _streamController.stream,
-      builder: (context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+    stream: _updateDataStreamController.stream,
+    builder: (context, AsyncSnapshot snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(
+          child: PokeballSpinner(),
+        );
+      } else if (snapshot.data?.length == 0) {
+        if (_isLoading) {
           return const Center(
             child: PokeballSpinner(),
           );
-        } else if (snapshot.data?.length == 0) {
-          if (_isLoading) {
-            return const Center(
-              child: PokeballSpinner(),
-            );
-          } else {
-            return const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Nothing was found for your query, try changing the search value.',
-                style: TextStyle(
-                    fontFamily: 'Plus Jakarta Sans',
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15,
-                    color: ColorConstants.descriptionGrey),
-              ),
-            );
-          }
-        } else if (snapshot.hasData) {
-          final view = _isGridEnabled
-              ? twinColumnGrid(snapshot)
-              : singleColumnGrid(snapshot);
-          return view;
-        } else if (snapshot.hasError) {
-          return Text('Error ${snapshot.hasData}');
         } else {
-          return Container();
+          return const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Nothing was found for your query, try changing the search value.',
+              style: TextStyle(
+                  fontFamily: 'Plus Jakarta Sans',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                  color: ColorConstants.descriptionGrey),
+            ),
+          );
         }
-      });
+      } else if (snapshot.hasData) {
+        final view = _isGridEnabled
+            ? twinColumnGrid(snapshot)
+            : singleColumnGrid(snapshot);
+        return view;
+      } else if (snapshot.hasError) {
+        return Text('Error ${snapshot.hasData}');
+      } else {
+        return Container();
+      }
+    }
+  );
 
   Widget twinColumnGrid(snapshot) => RefreshIndicator(
-        onRefresh: pullToRefresh,
-        child: GridView.builder(
-            controller: _scrollController,
-            padding: const EdgeInsets.all(16),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 0.7,
-              mainAxisSpacing: 9,
-              crossAxisSpacing: 9,
-            ),
-            itemCount: snapshot.data?.length + 2,
-            itemBuilder: (context, index) {
-              if (index < snapshot.data?.length) {
-                return TwinColumnCell(snapshot: snapshot, index: index);
-              } else {
-                if (snapshot.data?.length <
-                        paginationService.currentList.length &&
-                    snapshot.data?.length != 0) {
-                  return const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Center(
-                      child: PokeballSpinner(),
-                    ),
-                  );
-                } else {
-                  return null;
-                }
-              }
-            }),
-      );
+    onRefresh: pullToRefresh,
+    child: GridView.builder(
+      controller: _scrollController,
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.7,
+        mainAxisSpacing: 9,
+        crossAxisSpacing: 9,
+      ),
+      itemCount: snapshot.data?.length + 2,
+      itemBuilder: (context, index) {
+        if (index < snapshot.data?.length) {
+          return TwinColumnCell(snapshot: snapshot, index: index);
+        } else {
+          if (snapshot.data?.length <
+                  _paginationService.currentList.length &&
+              snapshot.data?.length != 0) {
+            return const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: PokeballSpinner(),
+              ),
+            );
+          } else {
+            return null;
+          }
+        }
+      }
+    ),
+  );
 
   Widget singleColumnGrid(snapshot) => RefreshIndicator(
-        onRefresh: pullToRefresh,
-        child: GridView.builder(
-            controller: _scrollController,
-            padding: EdgeInsets.all(16),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-              childAspectRatio: 2.7,
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-            ),
-            itemCount: snapshot.data?.length + 1,
-            itemBuilder: (context, index) {
-              if (index < snapshot.data?.length) {
-                return SingleColumnCell(snapshot: snapshot, index: index);
-              } else {
-                if (snapshot.data?.length <
-                        paginationService.currentList.length &&
-                    snapshot.data?.length != 0) {
-                  return const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Center(
-                      child: PokeballSpinner(),
-                    ),
-                  );
-                } else {
-                  return null;
-                }
-              }
-            }),
-      );
+    onRefresh: pullToRefresh,
+    child: GridView.builder(
+      controller: _scrollController,
+      padding: EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: 2.7,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+      ),
+      itemCount: snapshot.data?.length + 1,
+      itemBuilder: (context, index) {
+        if (index < snapshot.data?.length) {
+          return SingleColumnCell(snapshot: snapshot, index: index);
+        } else {
+          if (snapshot.data?.length <
+                  _paginationService.currentList.length &&
+              snapshot.data?.length != 0) {
+            return const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: PokeballSpinner(),
+              ),
+            );
+          } else {
+            return null;
+          }
+        }
+      }
+    ),
+  );
 
   Future<void> pullToRefresh() async {
     await resetData(null);
